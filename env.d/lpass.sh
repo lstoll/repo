832e5be116df2f62f99c717bfef034ff26b06684
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-05 11:22:26 -0700

    password helpers

---
+++ env.d/lpass.sh
@@ -0,0 +1,14 @@
+function lpass-ansible-vault-keyfile {
+    [ -z "$1" ] && echo "Item name not specified" && return 1
+    lpass show --password $1 > ~/.ansible/vaultkey.txt
+    [ $? == 0 ] || return 1
+    chmod 0600 ~/.ansible/vaultkey.txt
+    export VAULT_PASSWORD_FILE=$HOME/.ansible/vaultkey.txt
+}
+
+function lpass-aws-keys {
+    [ -z "$1" ] && echo "Item name not specified" && return 1
+    export AWS_ACCESS_KEY_ID=`lpass show --username $1`
+    [ $? == 0 ] || return 1
+    export AWS_SECRET_ACCESS_KEY=`lpass show --username $1`
+}
