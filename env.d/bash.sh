7062acf6301fd00d9be9af00ac4da3d762794715
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-08 23:32:03 -0700

    some bash history shizz

---
+++ env.d/bash.sh
@@ -0,0 +1,5 @@
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+
