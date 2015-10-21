4a621b60ccae13c1bb827c3691e77ccc71e548d8
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-21 10:06:36 -0400

    include go code too

--- env.d/git.sh
+++ env.d/git.sh
@@ -1,3 +1,3 @@
 function dirtysrc {
-    for d in $(find . -name .git -type d); do test -z "$(git -C $(dirname $d) status --porcelain)" || echo "$(dirname $d) has a dirty working copy"; done
+    for d in $(find ~/src ~/gocode/src -name .git -type d); do test -z "$(git -C $(dirname $d) status --porcelain)" || echo "$(dirname $d) has a dirty working copy"; done
 }
