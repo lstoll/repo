9ffa4eba8601e11ea1cd6015e889966d275c816f
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-21 19:59:02 -0400

    tweak

--- env.d/ssh.sh
+++ env.d/ssh.sh
@@ -2,7 +2,8 @@
 
 
 printf "# THIS FILE IS AUTO GENERATED, SEE ~/.ssh/config.in AND ~/.private_ssh_config\n\n" > ~/.ssh/config
-cat ~/.ssh/config.in > ~/.ssh/config
+cat ~/.ssh/config.in >> ~/.ssh/config
 if [ -f ~/.private_ssh_config ]; then
+    printf "\n\n#PRIVATE\n" >> ~/.ssh/config
     cat ~/.private_ssh_config >> ~/.ssh/config
 fi
