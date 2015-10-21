040077ad8e5c4332efdd5aa08cfc034446066f88
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-21 19:59:49 -0400

    whitespace

--- env.d/ssh.sh
+++ env.d/ssh.sh
@@ -1,9 +1,7 @@
 # Merge the files
-
-
 printf "# THIS FILE IS AUTO GENERATED, SEE ~/.ssh/config.in AND ~/.private_ssh_config\n\n" > ~/.ssh/config
 cat ~/.ssh/config.in >> ~/.ssh/config
 if [ -f ~/.private_ssh_config ]; then
-    printf "\n\n#PRIVATE\n" >> ~/.ssh/config
+    printf "\n\n# PRIVATE\n\n" >> ~/.ssh/config
     cat ~/.private_ssh_config >> ~/.ssh/config
 fi
