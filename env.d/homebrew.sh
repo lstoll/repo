e143671fb11a6c4b9e1259f09518d08c7a2c95a8
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-12-10 16:16:13 +1100

    guards for these existing

--- env.d/homebrew.sh
+++ env.d/homebrew.sh
@@ -1,3 +1,5 @@
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
+if type brew >/dev/null 2>&1; then
+    if [ -f `brew --prefix`/etc/bash_completion ]; then
+	. `brew --prefix`/etc/bash_completion
+    fi
 fi
