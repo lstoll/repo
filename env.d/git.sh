3b8d4457e5f3d218da52d3e812ed2de03501b004
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-02-09 08:43:52 -0800

    Use the go path style paths

--- env.d/git.sh
+++ env.d/git.sh
@@ -1,5 +1,5 @@
 function dirtysrc {
-    for d in $(find ~/src ~/gocode/src -name .git -type d); do test -z "$(git -C $(dirname $d) status --porcelain)" || echo "$(dirname $d) has a dirty working copy"; done
+    for d in $(find ~/src -name .git -type d); do test -z "$(git -C $(dirname $d) status --porcelain)" || echo "$(dirname $d) has a dirty working copy"; done
 }
 
 alias gup='git fetch origin && git rebase origin/$(git rev-parse --abbrev-ref HEAD)'
