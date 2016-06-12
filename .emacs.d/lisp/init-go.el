98a8397a0bd0c366e78f31fcc053b09acad4be6c
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-06-12 14:35:09 -0400

    Try gometalinter

--- .emacs.d/lisp/init-go.el
+++ .emacs.d/lisp/init-go.el
@@ -1,5 +1,6 @@
 (require-package 'go-mode)
 (require-package 'go-autocomplete)
+(require-package 'flycheck-gometalinter)
 
 ; dafuq is up with autoloads?
 ;(load "go-mode.el")
@@ -10,6 +11,19 @@
 ; Installed by thesetup
 (load-file (concat "/Users/" (getenv "USER") "/src/golang.org/x/tools/cmd/oracle/oracle.el"))
 (load-file (concat "/Users/" (getenv "USER") "/src/golang.org/x/tools/refactor/rename/go-rename.el"))
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-gometalinter-setup))
+
+;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+(setq flycheck-gometalinter-vendor t)
+;; disable linters
+;(setq flycheck-gometalinter-disable-linters '("gotype" "gocyclo"))
+;; Only enable selected linters
+;(setq flycheck-gometalinter-disable-all t)
+;(setq flycheck-gometalinter-enable-linters '("golint"))
+;; Set different deadline (default: 5s)
+;(setq flycheck-gometalinter-deadline "10s")
 
 (defun my-go-mode-hook ()
   ;; Use goimports instead of go-fmt
@@ -22,8 +36,5 @@
   (setq ac-sources '(ac-source-go ac-source-yasnippet))
   )
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-(setq-default flycheck-go-test-executable "~/.emacs.d/bin/go_godep_shim")
-(setq-default flycheck-go-build-executable "~/.emacs.d/bin/go_godep_shim")
 
 (provide 'init-go)
