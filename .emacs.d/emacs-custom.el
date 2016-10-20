47546e8f4fac703ae569274c3d6f96b326c16fa8
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-10-20 11:13:10 -0400

    Flycheck more visible

--- .emacs.d/emacs-custom.el
+++ .emacs.d/emacs-custom.el
@@ -3,11 +3,47 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#cccccc" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#515151"))
  '(coffee-tab-width 2)
- '(flycheck-navigation-minimum-level (quote error)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fci-rule-color "#515151")
+ '(flycheck-navigation-minimum-level (quote error))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-error ((t (:underline (:color "firebrick" :style wave)))))
+ '(flycheck-fringe-error ((t (:foreground "firebrick"))))
+ '(flycheck-fringe-info ((t (:foreground "forest green"))))
+ '(flycheck-fringe-warning ((t (:foreground "orange"))))
+ '(flycheck-info ((t (:underline (:color "forest green" :style wave)))))
+ '(flycheck-warning ((t (:underline (:color "dark goldenrod" :style wave))))))
