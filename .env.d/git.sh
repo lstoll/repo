5d4f70ae85d148c48d90132db11bba8e73ff0b2f
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-10-20 11:15:35 -0400

    prune always

--- .env.d/git.sh
+++ .env.d/git.sh
@@ -2,4 +2,4 @@ function dirtysrc {
     for d in $(find ~/src -name .git -type d); do test -z "$(git -C $(dirname $d) status --porcelain)" || echo "$(dirname $d) has a dirty working copy"; done
 }
 
-alias gup='git fetch origin && git rebase origin/$(git rev-parse --abbrev-ref HEAD)'
+alias gup='git fetch origin --prune && git rebase origin/$(git rev-parse --abbrev-ref HEAD)'
