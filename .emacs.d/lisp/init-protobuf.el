fd20d6dc8634ac63154930cecd63f438b9e8bf4d
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-10-22 00:59:54 -0400

    Add imenu for protobuf

--- .emacs.d/lisp/init-protobuf.el
+++ .emacs.d/lisp/init-protobuf.el
@@ -1,4 +1,12 @@
 (require-package 'protobuf-mode)
 (require-package 'flycheck-protobuf)
 
+(setq protobuf-imenu-generic-expression
+      '(("service" "^service *\\(.*\\) {" 1)
+	("message" "^message *\\(.*\\) {" 1)))
+
+(add-hook 'protobuf-mode-hook
+	  (lambda ()
+	    (setq imenu-generic-expression protobuf-imenu-generic-expression)))
+
 (provide 'init-protobuf)
